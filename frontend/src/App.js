e233f9dcc1e38c68a9c073fce694ccdb574c035d
Dipesh Ss <dipeshshivrame@gmail.com>
2020-11-24 20:54:21 +0530

    front end auth 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import { Container } from 'react-bootstrap'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
 
 function App() {
   return (
@@ -18,9 +19,10 @@ function App() {
 
         <main className="py-3">
           <Container> 
-            <Route path="/" component={HomeScreen} exact />
+            <Route path='/login' component={LoginScreen} />
             <Route path='/product/:id' component={ProductScreen}/>
             <Route path="/cart/:id?" component={CartScreen} />
+            <Route path="/" component={HomeScreen} exact />
           </Container>
         </main>
 
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,22 +4,27 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import {
      productDetailsReducer,
      productListReducer
-}
-     from './reducers/productReducers'
+} from './reducers/productReducers'
 
+import { userLoginReducer } from './reducers/userReducers'
 import { cartReducer } from './reducers/cartReducers'
 
 const reducer = combineReducers({
      productList: productListReducer,
      productDetail: productDetailsReducer,
-     cart: cartReducer
+     cart: cartReducer,
+     userLogin: userLoginReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ?
      JSON.parse(localStorage.getItem('cartItems')) : []
 
+const userInfoFromStorage = localStorage.getItem('userInfo') ?
+     JSON.parse(localStorage.getItem('userInfo')) : null
+
 const initialState = {
-     cart: { cartItems : cartItemsFromStorage }
+     cart: { cartItems: cartItemsFromStorage },
+     userLogin : { userInfo : userInfoFromStorage }
 }
 
 const middleware = [thunk]
